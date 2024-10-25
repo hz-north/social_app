775bb7f0c3d9709e7f22963a41e231204c34c7c1
morty-9 <hzawnineseven@gmail.com>
2024-10-25 16:59:24 +0630

    env

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -1,4 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Link } from "react-router-dom";
 import {
   Form,
   FormDescription,
@@ -13,8 +14,11 @@ import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
 import { SignupValidation } from "@/lib/validation";
 import { z } from "zod";
+import Loader from "@/components/shared/loader";
 
 const SignUpForm = () => {
+  const isLoading = true;
+
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
     defaultValues: {
@@ -26,20 +30,18 @@ const SignUpForm = () => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    // create user
+    //const newUser = await createUserAccount(values);
   }
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
         <img src="/assets/images/logo.svg" alt="logo" />
-        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12"></h2>
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
         <p className="text-light-3 small-medium md:base-regular mt-2">
           To use soxial enter your account details
         </p>
-
         <form
           onSubmit={form.handleSubmit(onSubmit)}
           className="flex flex-col gap-5 w-full mt-4"
@@ -97,8 +99,13 @@ const SignUpForm = () => {
             )}
           />
           <Button type="submit" className="shad-button_primary">
-            Submit
+            {isLoading ? (
+              <div className="flex-center gap-2"> <Loader/>  Loading...</div>
+            ): "Sign Up"}
           </Button>
+          <p className="text-small-regular text-light-2 text-center mt-2">Already have an account
+            <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">Log In</Link>
+          </p>
         </form>
       </div>
     </Form>
---
+++ src/lib/appwrite/config.ts
@@ -0,0 +1,5 @@
+import { Client, Account, Databses, Storage, Avatars } from 'appwrite';
+
+export const appwriteConfig = {
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+}
